3ab3836cbfd2c4f8cb492c4eee37bd2c17bb9aef
YeonghyeonKO <46114393+YeonghyeonKO@users.noreply.github.com>
2019-10-15 22:45:07 +0900

    Create 자율과제1.py

---
+++ 코알라univ/Week 2/자율과제1.py
@@ -0,0 +1,14 @@
+print("일반계산기 프로그램입니다!")
+one = int(input("계산할 첫 번째 값을 입력해주세요 : "))
+two = int(input("계산할 첫 번째 값을 입력해주세요 : "))
+print("\n")
+
+print("두 개의 값 : ", one, "와", two, "\n")
+
+print("더하기 값 (a + b) : ", one + two)
+print("빼기 값 (a - b) : ", one - two)
+print("곱하기 값 (a * b) : ", one * two)
+print("정수 나누기 값 (a // b) : ", one // two)
+print("실수 나누기 값 (a // b) : ", one / two)
+print("정수 나누기 값 (a % b) : ", one % two)
+
