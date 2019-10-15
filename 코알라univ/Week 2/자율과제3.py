021364fbd651cb10b24892d74ab8221f56a47ae9
YeonghyeonKO <46114393+YeonghyeonKO@users.noreply.github.com>
2019-10-15 22:45:02 +0900

    Create 자율과제3.py

---
+++ 코알라univ/Week 2/자율과제3.py
@@ -0,0 +1,18 @@
+for i in range(10):
+    print("*" * i)
+
+print("\n")
+
+for i in range(6):
+    print("*" * (2 * i-1))
+
+# for i in range(1, 11, 2):
+#    print("*" * i)
+
+print("\n")
+
+for i in range(10):
+    print("*" * (10 - i))
+
+# for i in range(1, 11):
+#    print("*" * (11 - i))
