b2ce77e4c6568aa3e6057ea0d98a5ef8b3824c5e
YeonghyeonKO <46114393+YeonghyeonKO@users.noreply.github.com>
2019-10-15 22:44:43 +0900

    Create 6. Relational Data.r

---
+++ 전산통계와 실험/6. Relational Data.r
@@ -0,0 +1,56 @@
+library(nycflights13); library(tidyverse)
+
+flights
+
+airlines; airports; planes; weather
+
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  3, "x3"
+)
+
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2",
+  4, "y3"
+)
+
+inner_join(x, y, by = "key")
+left_join(x, y, by = "key")
+right_join(x, y, by = "key")
+full_join(x, y, by = "key")
+
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  2, "x3",
+  1, "x4"
+)
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2"
+)
+left_join(x, y, by = "key")
+
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  2, "x3",
+  3, "x4"
+)
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2",
+  2, "y3",
+  3, "y4"
+)
+left_join(x, y, by = "key")
+
+
